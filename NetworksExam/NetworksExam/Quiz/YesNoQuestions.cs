bf3f9e4e1f25a66e12ada15783d2cfe65d37b140
Robin Nunkesser <robin.nunkesser@hshl.de>
2021-10-05 14:09:10 +0200

    Version 0.5.0

--- NetworksExam/NetworksExam/Quiz/YesNoQuestions.cs
+++ NetworksExam/NetworksExam/Quiz/YesNoQuestions.cs
@@ -13,13 +13,28 @@ namespace NetworksExam.Quiz
             },
             new YesNoQuestion()
             {
-                Text = @"Bei bekannter IP-Adresse eines Servers können wir mit dessen Prozessen kommunizieren.",
+                Text = @"Die Mindestlänge von Rahmen reicht um Kollisionen zu vermeiden.",
                 Answer = false
             },
             new YesNoQuestion()
             {
+                Text = @"Bei bekannter IP-Adresse eines Servers können wir mit dessen Prozessen kommunizieren.",
+                Answer = false,
+                AlternativeQuestion = new YesNoQuestion()
+                {
+                    Text = @"Bei bekannter IP-Adresse eines Webservers können wir Webseiten anfragen.",
+                    Answer = true
+                }
+            },
+            new YesNoQuestion()
+            {
                 Text = @"UDP ist für Videotelefonie besser geeignet als TCP.",
-                Answer = true
+                Answer = true,
+                AlternativeQuestion = new YesNoQuestion()
+                {
+                    Text = @"TCP ist für Videotelefonie besser geeignet als UDP.",
+                    Answer = false
+                }
             },
             new YesNoQuestion()
             {
@@ -29,47 +44,92 @@ namespace NetworksExam.Quiz
             new YesNoQuestion()
             {
                 Text = @"Die Netzwerktopologie Maschen enthält einen Single Point of Failure.",
-                Answer = false
+                Answer = false,
+                AlternativeQuestion = new YesNoQuestion()
+                {
+                    Text = @"Die Netzwerktopologie Bus enthält einen Single Point of Failure.",
+                    Answer = true
+                }
             },
             new YesNoQuestion()
             {
                 Text = @"Router untersuchen Rahmen mit Prüfsummen auf Korrektheit.",
-                Answer = false
+                Answer = false,
+                AlternativeQuestion = new YesNoQuestion()
+                {
+                    Text = @"Bridges untersuchen Rahmen mit Prüfsummen auf Korrektheit.",
+                    Answer = true
+                }
             },
             new YesNoQuestion()
             {
                 Text = @"Bridges vermeiden Kreise mit dem Spanning Tree Protocol.",
-                Answer = true
+                Answer = true,
+                AlternativeQuestion = new YesNoQuestion()
+                {
+                    Text = @"Router vermeiden Kreise mit dem Spanning Tree Protocol.",
+                    Answer = false
+                }
             },
             new YesNoQuestion()
             {
                 Text = @"Der Leitungscode Non-Return-To-Zero (NRZ) verursacht Probleme bei der Taktwiederherstellung.",
-                Answer = true
+                Answer = true,
+                AlternativeQuestion = new YesNoQuestion()
+                {
+                    Text = @"Der Leitungscode Manchester verursacht Probleme bei der Taktwiederherstellung.",
+                    Answer = false
+                }
             },
             new YesNoQuestion()
             {
                 Text = @"Gateways in der Vermittlungsschicht von Computernetzen werden häufig eingesetzt.",
-                Answer = false
+                Answer = false,
+                AlternativeQuestion = new YesNoQuestion()
+                {
+                    Text = @"Gateways in der Vermittlungsschicht von Computernetzen sind heutzutage selten nötig.",
+                    Answer = true
+                }
             },
             new YesNoQuestion()
             {
                 Text = @"Telnet ermöglicht die verschlüsselte Fernsteuerung von Computern.",
-                Answer = false
+                Answer = false,
+                AlternativeQuestion = new YesNoQuestion()
+                {
+                    Text = @"Telnet ermöglicht die unverschlüsselte Fernsteuerung von Computern.",
+                    Answer = true
+                }
             },
             new YesNoQuestion()
             {
                 Text = @"DHCP ermöglicht die Zuweisung der Netzwerkkonfiguration an Netzwerkgeräte.",
-                Answer = true
+                Answer = true,
+                AlternativeQuestion = new YesNoQuestion()
+                {
+                    Text = @"ICMP ermöglicht die Zuweisung der Netzwerkkonfiguration an Netzwerkgeräte.",
+                    Answer = false
+                }
             },
             new YesNoQuestion()
             {
                 Text = @"ICMP dient dazu, Diagnose- und Fehlermeldungen auszutauschen.",
-                Answer = true
+                Answer = true,
+                AlternativeQuestion = new YesNoQuestion()
+                {
+                    Text = @"DHCP dient dazu, Diagnose- und Fehlermeldungen auszutauschen.",
+                    Answer = false
+                }
             },
             new YesNoQuestion()
             {
                 Text = @"Im Ethernet-Rahmen stehen die MAC-Adressen von Sender und Empfänger.",
-                Answer = true
+                Answer = true,
+                AlternativeQuestion = new YesNoQuestion()
+                {
+                    Text = @"Im Ethernet-Rahmen stehen die IP-Adressen von Sender und Empfänger.",
+                    Answer = false
+                }
             }
             };
 
