5c1ce720a000eba082e79420d01a22a17537ba81
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-03-24 10:19:36 +0100

    0.8.1

--- Italbytz.Adapters.Exam.Networks/Italbytz.Adapters.Exam.Networks/Graph/UndirectedGraph.cs
+++ Italbytz.Adapters.Exam.Networks/Italbytz.Adapters.Exam.Networks/Graph/UndirectedGraph.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using Italbytz.Ports.Exam.Networks;
+using QuikGraph.Graphviz;
+using QuikGraph.Graphviz.Dot;
 
 namespace Italbytz.Adapters.Exam.Networks.Graph
 {
@@ -11,5 +13,29 @@ namespace Italbytz.Adapters.Exam.Networks.Graph
         }
 
         public IEnumerable<TEdge> Edges { get; set; }
+
+        public string ToGraphviz()
+        {
+            if (typeof(TVertex) == typeof(string) && typeof(TEdge) == typeof(ITaggedEdge<string, double>))
+            {
+                var graph = ((IUndirectedGraph<string, ITaggedEdge<string, double>>)this).ToQuikGraph();
+                return graph.ToGraphviz(algorithm =>
+                {
+                    algorithm.GraphFormat.RankDirection = GraphvizRankDirection.LR;
+                    algorithm.FormatVertex += (sender, args) =>
+                    {
+                        args.VertexFormat.Label = $"{args.Vertex}";
+                    };
+                    algorithm.FormatEdge += (sender, args) =>
+                    {
+                        if (args.Edge is QuikGraph.TaggedEdge<string, double> edge)
+                        {
+                            args.EdgeFormat.Label.Value = $"{edge.Tag}";
+                        }
+                    };
+                });
+            }
+            return "";
+        }
     }
 }
